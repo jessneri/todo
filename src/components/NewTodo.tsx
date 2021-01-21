8a1c116c28afa745184d0bee6435f6986a26ee34
Jessica Neri <jess.neri94@gmail.com>
2021-01-21 13:52:40 -0300

    Simple to-do app with ts

---
+++ src/components/NewTodo.tsx
@@ -0,0 +1,28 @@
+import React, { useRef } from 'react';
+
+import './NewTodo.css'
+;
+type NewTodoProps = {
+    onAddTodo: (todoText: string) => void;
+}
+
+const NewTodo: React.FC<NewTodoProps> = props => {
+    const textInputRef = useRef<HTMLInputElement>(null);
+
+    const todoSubmitHandler = (event: React.FormEvent) => {
+        event.preventDefault();
+        const enteredText = textInputRef.current!.value;
+        props.onAddTodo(enteredText)
+    }
+
+
+    return <form onSubmit={todoSubmitHandler}>
+        <div>
+            <label htmlFor="todo-text">To-do:</label>
+            <input type="text" id="todo-text" ref={textInputRef}/>
+        </div>
+        <button type="submit">Add</button>
+    </form>
+};
+
+export default NewTodo;
